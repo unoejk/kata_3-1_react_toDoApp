ea8978df162085d17b6b747d40d4e39c090a5b35
Leonid Burtsev <unoejk@gmail.com>
2024-03-12 19:00:26 +0300

    init project

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+
+// ---- import
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import './App.css'
+
+
+// ---- go-go
+
+export default class extends React.Component{
+    render() {return(
+        <h1>Hello there</h1>
+    )}
+}
