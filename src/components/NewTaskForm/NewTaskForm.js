2e0e7ba18575c051b657f36a67d65775b1bd5a4c
Leonid Burtsev <unoejk@gmail.com>
2024-03-24 00:53:50 +0300

    .\.git\

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -4,57 +4,86 @@ import React from 'react'
 import './NewTaskForm.css'
 
 export default class NewTaskForm extends React.Component {
-  static defaultProps = {
-    addTask: () => {},
-  }
-  static propTypes = {
-    addTask: (props, propName, componentName) => {
-      if (typeof props[propName] === 'function') return null
-      return new TypeError(`${componentName}: ${propName} must be function`)
-    },
-  }
-
-  state = {
-    quest: '',
-    placeholder: 'What needs to be done?',
-  }
-
-  // addTask
-  onChange = (e) => {
-    this.setState({
-      quest: e.target.value,
-    })
-  }
-  onKeyUp = (e) => {
-    if (e.key === 'Enter') {
-      if (this.state.quest.trim() === '') {
-        this.setState({
-          quest: '',
-          placeholder: 'Require to write task',
-        })
-      } else {
-        this.props.addTask(this.state.quest)
-        this.setState({
-          quest: '',
-          placeholder: 'What needs to be done?',
-        })
-      }
+    static defaultProps = {
+        addTask: () => {
+        },
     }
-  }
+    static propTypes = {
+        addTask: (props, propName, componentName) => {
+            if (typeof props[propName] === 'function') return null
+            return new TypeError(`${componentName}: ${propName} must be function`)
+        },
+    }
 
-  render() {
-    return (
-      <header className="header">
-        <h1>Enter = Submit</h1>
-        <input
-          className="new-todo"
-          placeholder={this.state.placeholder}
-          autoFocus
-          value={this.state.quest}
-          onChange={this.onChange}
-          onKeyUp={this.onKeyUp}
-        />
-      </header>
-    )
-  }
+    state = {
+        quest: '',
+        min: '',
+        sec: '',
+    }
+
+    // addTask
+    onChange = (e,name) => {
+        this.setState({
+            [name]: e.target.value,
+        })
+    }
+    onKeyUp = (e) => {
+        if (e.key === 'Enter') {
+            if (this.state.quest.trim() !== '') {
+                let secTimer=0
+                let min=Math.floor(this.state.min)
+                let sec=Math.floor(this.state.sec)
+                if(
+                    min>=0
+                    && sec>=0
+                    && sec<=60
+                    && min*60+sec<=60*60
+                ){
+                    secTimer=(min*60+sec)
+                }
+                this.props.addTask(this.state.quest,secTimer)
+            }
+            this.setState({
+                quest: '',
+                min: '',
+                sec: '',
+            })
+        }
+    }
+
+    render() {
+        return (
+            <header className="header">
+                <h1>Enter = Submit</h1>
+                <form className="new-todo-form">
+                    <input
+                        className="new-todo"
+                        placeholder="Task"
+                        autoFocus
+                        value={this.state.quest}
+                        onChange={(e)=>{this.onChange(e,'quest')}}
+                        onKeyUp={this.onKeyUp}
+                    />
+                    <input
+                        className="new-todo-form__timer"
+                        placeholder="Min"
+                        // autoFocus
+                        value={this.state.min}
+                        onChange={(e)=>{this.onChange(e,'min')}}
+                        onKeyUp={this.onKeyUp}
+                        type={'number'} min={0} max={60} step={1}
+                    />
+                    <input
+                        className="new-todo-form__timer"
+                        placeholder="Sec"
+                        // autoFocus
+                        value={this.state.sec}
+                        onChange={(e)=>{this.onChange(e,'sec')}}
+                        onKeyUp={this.onKeyUp}
+                        type={'number'} min={0} max={60} step={1}
+                    />
+                </form>
+            </header>
+        )
+    }
 }
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -5,110 +5,177 @@ import { formatDistanceToNowStrict } from 'date-fns'
 import './Task.css'
 
 export default class Task extends React.Component {
-  static defaultProps = {
-    quest: '',
-    time: new Date(),
-    isCompleted: false,
-    completeTask: () => {},
-    changeTask: () => {},
-    removeTask: () => {},
-  }
-  static propTypes = {
-    quest: (props, propName, componentName) => {
-      if (typeof props[propName] === 'string') return null
-      return new TypeError(`${componentName}: ${propName} must be string`)
-    },
-    time: (props, propName, componentName) => {
-      if (props[propName] instanceof Date) return null
-      return new TypeError(`${componentName}: ${propName} must be Date`)
-    },
-    isCompleted: (props, propName, componentName) => {
-      if (typeof props[propName] === 'boolean') return null
-      return new TypeError(`${componentName}: ${propName} must be boolean`)
-    },
-    completeTask: (props, propName, componentName) => {
-      if (typeof props[propName] === 'function') return null
-      return new TypeError(`${componentName}: ${propName} must be function`)
-    },
-    changeTask: (props, propName, componentName) => {
-      if (typeof props[propName] === 'function') return null
-      return new TypeError(`${componentName}: ${propName} must be function`)
-    },
-    removeTask: (props, propName, componentName) => {
-      if (typeof props[propName] === 'function') return null
-      return new TypeError(`${componentName}: ${propName} must be function`)
-    },
-  }
-
-  state = {
-    isEditing: false,
-    quest: '',
-  }
-
-  taskStyleClass = () => {
-    let res = []
-    if (this.props.isCompleted) res.push('completed')
-    if (this.state.isEditing) res.push('editing')
-    return res.join(' ')
-  }
-
-  timeString = 'created ' + formatDistanceToNowStrict(this.props.time, { addSuffix: true })
-
-  switchChange = () => {
-    this.setState((state) => {
-      return {
-        isEditing: !state.isEditing,
-      }
-    })
-  }
-
-  completeTask = () => {
-    this.props.completeTask(this.props.isCompleted)
-  }
-
-  removeTask = () => {
-    this.props.removeTask()
-  }
-
-  // changeTask
-  questOnChange = (e) => {
-    if (e.target.value.trim() !== '') {
-      this.props.changeTask(e.target.value)
-    }
-    this.setState({
-      quest: e.target.value,
-    })
-  }
-  questOnKeyUp = (e) => {
-    if (e.key === 'Enter') {
-      this.switchChange()
-      this.setState({
+    static defaultProps = {
         quest: '',
-      })
+        creationData: new Date(),
+        secTimer:0,
+        isCompleted: false,
+        completeTask: () => {
+        },
+        changeTask: () => {
+        },
+        removeTask: () => {
+        },
     }
-  }
+    static propTypes = {
+        quest: (props, propName, componentName) => {
+            if (typeof props[propName] === 'string') return null
+            return new TypeError(`${componentName}: ${propName} must be string`)
+        },
+        creationData: (props, propName, componentName) => {
+            if (props[propName] instanceof Date) return null
+            return new TypeError(`${componentName}: ${propName} must be Date`)
+        },
+        secTimer: (props, propName, componentName) => {
+            if (typeof props[propName] === 'number') return null
+            return new TypeError(`${componentName}: ${propName} must be number`)
+        },
+        isCompleted: (props, propName, componentName) => {
+            if (typeof props[propName] === 'boolean') return null
+            return new TypeError(`${componentName}: ${propName} must be boolean`)
+        },
+        completeTask: (props, propName, componentName) => {
+            if (typeof props[propName] === 'function') return null
+            return new TypeError(`${componentName}: ${propName} must be function`)
+        },
+        changeTask: (props, propName, componentName) => {
+            if (typeof props[propName] === 'function') return null
+            return new TypeError(`${componentName}: ${propName} must be function`)
+        },
+        removeTask: (props, propName, componentName) => {
+            if (typeof props[propName] === 'function') return null
+            return new TypeError(`${componentName}: ${propName} must be function`)
+        },
+    }
 
-  render() {
-    return (
-      <li className={this.taskStyleClass()}>
-        <div className="view">
-          <input className="toggle" type="checkbox" checked={this.props.isCompleted} onChange={this.completeTask} />
-          <label>
-            <span className="description">{this.props.quest}</span>
-            <span className="created">{this.timeString}</span>
-          </label>
-          <button className="icon icon-edit" onClick={this.switchChange}></button>
-          <button className="icon icon-destroy" onClick={this.removeTask}></button>
-        </div>
-        <input
-          type="text"
-          className="edit"
-          placeholder="Editing task"
-          value={this.state.quest}
-          onChange={this.questOnChange}
-          onKeyUp={this.questOnKeyUp}
-        />
-      </li>
-    )
-  }
+    state = {
+        quest: this.props.quest,
+        secTimerBalance:this.props.secTimer,
+        isEditing: false,
+        isCounting: false,
+        updateFlag:0,
+    }
+
+    componentDidMount(){
+        setInterval(this.updateSecTimerBalance,1000)
+    }
+
+    taskStyleClass = () => {
+        let res = []
+        if (this.props.isCompleted) res.push('completed')
+        if (this.state.isEditing) res.push('editing')
+        return res.join(' ')
+    }
+
+    switchChange = () => {
+        this.setState((state) => {
+            return {
+                isEditing: !state.isEditing,
+            }
+        })
+    }
+
+    completeTask = () => {
+        this.props.completeTask(this.props.isCompleted)
+    }
+
+    removeTask = () => {
+        this.props.removeTask()
+    }
+
+    // changeTask
+    questOnChange = (e) => {
+        if (e.target.value.trim() !== '') {
+            this.props.changeTask(e.target.value)
+        }
+        this.setState({
+            quest: e.target.value,
+        })
+    }
+    questOnKeyUp = (e) => {
+        if (e.key === 'Enter') {
+            this.switchChange()
+            this.setState({
+                quest: '',
+            })
+        }
+    }
+
+    // time
+    getCreationDataString=()=>{
+        return 'created ' + formatDistanceToNowStrict(this.props.creationData, { addSuffix: true })
+    }
+    getSecTimerString=()=>{
+        let res=''
+        let rem=this.state.secTimerBalance
+        res+=Math.floor(rem/(10*60))
+        rem=rem%(10*60)
+        res+=Math.floor(rem/60)
+        rem=rem%60
+        res+=':'
+        res+=Math.floor(rem/10)
+        rem=rem%10
+        res+=rem
+        return res
+    }
+    updateSecTimerBalance=()=>{
+        this.setState({
+            updateFlag:this.state.updateFlag++,
+        })
+        if (this.state.isCounting){
+            if (this.state.secTimerBalance===0){
+                this.setState({
+                    isCounting:false,
+                })
+            }
+            this.setState({
+                secTimerBalance:this.state.secTimerBalance--,
+            })
+        }
+    }
+    switchTimer = (bool) => {
+        this.setState((state) => {
+            return {
+                isCounting: bool,
+            }
+        })
+    }
+
+    render() {
+        return (
+            <li className={this.taskStyleClass()}>
+                <div className="view">
+                    <input className="toggle" type="checkbox" checked={this.props.isCompleted}
+                           onChange={this.completeTask} />
+                    <label>
+                        <span className="title">{this.props.quest}</span>
+                        <span className="description">
+                          <button
+                              className="icon icon-play"
+                              onClick={()=>{this.switchTimer(true)}}
+                              disabled={this.state.secTimerBalance===0}
+                          ></button>
+                          <button
+                              className="icon icon-pause"
+                              onClick={()=>{this.switchTimer(false)}}
+                              disabled={this.state.secTimerBalance===0}
+                          ></button>
+                            {this.getSecTimerString()}
+                        </span>
+                        <span className="description">{this.getCreationDataString()}</span>
+                    </label>
+                    <button className="icon icon-edit" onClick={this.switchChange}></button>
+                    <button className="icon icon-destroy" onClick={this.removeTask}></button>
+                </div>
+                <input
+                    type="text"
+                    className="edit"
+                    placeholder="Editing task"
+                    value={this.state.quest}
+                    onChange={this.questOnChange}
+                    onKeyUp={this.questOnKeyUp}
+                />
+            </li>
+        )
+    }
 }
